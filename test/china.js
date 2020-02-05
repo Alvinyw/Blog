4435b60f5a9d551740b8b344a27b366cbb7b9eb8
Alvin <alvin.yiwen@gmail.com>
2020-02-05 12:27:20 +0800

    add echarts china.js

---
+++ test/china.js
@@ -0,0 +1,165 @@
+var china = [
+    {
+        name: '江西', // 省份
+        cure: 5,  // 治愈人数
+        death: 2, // 死亡人数
+        confirm: 53, // 确证人数
+        city: [
+            {
+                name: '南昌',
+                cure: 5,
+                death: 2,
+                confirm: 53,
+            },
+            {
+                name: '赣州',
+                cure: 5,
+                death: 2,
+                confirm: 53,
+            }
+        ]
+    },
+    {
+        name: '北京',
+        cure: 5,
+        death: 2,
+        confirm: 38
+    },
+    {
+        name: '上海',
+        cure: 5,
+        death: 2,
+        confirm: 46
+    },
+    {
+        name: '重庆',
+        cure: 5,
+        death: 2,
+        confirm: 36
+    },
+    {
+        name: '河北',
+        cure: 5,
+        death: 2,
+        confirm: 34
+    },
+    {
+        name: '河南',
+        cure: 5,
+        death: 2,
+        confirm: 32
+    },
+    {
+        name: '云南',
+        cure: 5,
+        death: 2,
+        confirm: 16
+    },
+    {
+        name: '辽宁',
+        cure: 5,
+        death: 2,
+        confirm: 43
+    },
+    {
+        name: '黑龙江',
+        cure: 5,
+        death: 2,
+        confirm: 42
+    },
+    {
+        name: '湖南',
+        cure: 5,
+        death: 2,
+        confirm: 24
+    },
+    {
+        name: '安徽',
+        cure: 5,
+        death: 2,
+        confirm: 33
+    },
+    {
+        name: '山东',
+        cure: 5,
+        death: 2,
+        confirm: 32
+    },
+    {
+        name: '新疆',
+        cure: 5,
+        death: 2,
+        confirm: 2
+    },
+    {
+        name: '江苏',
+        cure: 5,
+        death: 2,
+        confirm: 39
+    },
+    {
+        name: '浙江',
+        cure: 5,
+        death: 2,
+        confirm: 35
+    },
+    {
+        name: '江西',
+        cure: 5,
+        death: 2,
+        confirm: 22
+    },
+    {
+        name: '湖北',
+        cure: 5,
+        death: 2,
+        confirm: 22
+    },
+    {
+        name: '广西',
+        cure: 5,
+        death: 2,
+        confirm: 32
+    },
+    {
+        name: '甘肃',
+        cure: 5,
+        death: 2,
+        confirm: 12
+    },
+    {
+        name: '山西',
+        cure: 5,
+        death: 2,
+        confirm: 32
+    },
+    {
+        name: '内蒙古',
+        cure: 5,
+        death: 2,
+        confirm: 35
+    },
+    {
+        name: '陕西',
+        cure: 5,
+        death: 2,
+        confirm: 25
+    },
+    {
+        name: '吉林',
+        cure: 5,
+        death: 2,
+        confirm: 45
+    },
+    {
+        name: '福建',
+        cure: 5,
+        death: 2,
+        confirm: 28
+    },
+    {
+        name: '贵州'
+    }
+]
+
+export default china
