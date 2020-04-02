5d4b122b33d7967f7465bec3416bb79f67545cf1
Alvinyw <alvin.yiwen@gmail.com>
2020-04-02 10:51:28 +0800

    调整群居 loading 方法的祖册位置

--- imageViewer_vue/src/components/install.js
+++ imageViewer_vue/src/components/install.js
@@ -1,14 +1,23 @@
 // 组件全局注册
 import Vue from 'vue'
 
+// 注册全局组件
 import VanLoading from './VanLoading'
 
-// 组件库
 const Components = [VanLoading]
 
-// 注册全局组件
 Components.map(com => {
   Vue.component(com.name, com)
+})
+
+
+// 祖册全局方法
+import Loading from './Loading/loading'
+
+const globalMethods = [Loading]
+
+globalMethods.map(method => {
+  Vue.use(method)
 })
 
 export default Vue
